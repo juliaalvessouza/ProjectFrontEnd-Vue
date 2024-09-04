36be0846180c8d7d940ba18eff79d45cc8709a3a
Julia Souza <julia.souza@ciag.org.br>
2024-09-04 09:46:25 -0300

    select_list

--- cookin-up/src/components/Banner.vue
+++ cookin-up/src/components/Banner.vue
@@ -1,17 +1,20 @@
 <template>
-    <header class=banner>
-        <div class=apresentacao>
-          <img src="../assets/imagens/logo.svg" alt="Logo do Cookin'Up" class="logo">
-          <p class="cabecalho-lg frase-cabecalho">
-            <span class="texto-verde">Um banquete de ideias para </span>despertar o chef que já em você!
-          </p>
-          <p class="subtitulo-lg">
-            Explore novas receitas todos os dias com os ingredientes que estão ao seu alcance!
-          </p>
-        </div>
-    
-        <img src="../assets/imagens/foto-banner.png" alt="Foto de uma mulher cozinhando com uma bacia de vidros nas mãos. Ela está sorrindo e está apoiando um celular entre o rosto e ombro." class="foto-banner">
-      </header>
+  <header class="banner">
+    <div class="apresentacao">
+      <img src="../assets/imagens/logo.svg" alt="Logo do Cookin' Up" class="logo">
+
+      <p class="cabecalho-lg frase-cabecalho">
+        <span class="texto-verde">Um banquete de ideias para</span>
+        despertar o chef que há em você!
+      </p>
+
+      <p class="subtitulo-lg">
+        Explore novas receitas todos os dias com os ingredientes que estão ao seu alcance!
+      </p>
+    </div>
+
+    <img src="../assets/imagens/foto-banner.png" alt="Foto de uma mulher cozinhando com uma bacia de vidro nas mãos. Ela está sorrindo e está apoiando um celular entre seu rosto e ombro." class="foto-banner">
+  </header>
 </template>
 
 <style scoped>
--- cookin-up/src/interfaces/IReceita.ts
+++ cookin-up/src/interfaces/IReceita.ts
@@ -1,4 +1,4 @@
-export default interface ICategoria {
+export default interface IReceita {
   nome: string;
   ingredientes: string[];
   imagem: string;
